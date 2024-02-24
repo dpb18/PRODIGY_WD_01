9a4ad47aa92a3b022320a6f42d10254715933753
dpb18 <145085626+dpb18@users.noreply.github.com>
2024-02-25 00:46:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+  
+  function openNav1() {
+    document.getElementById("mySidebar").style.width = "450px";
+    document.getElementById("main").style.marginLeft = "450px";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav1() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
